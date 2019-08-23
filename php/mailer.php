1c968eb36f71c4888ec7f2a48821795307ebe65b
Jrodl3r <john@jrod.io>
2019-08-22 22:57:37 -0700

    cont. migrating to google cloud

--- php/mailer.php
+++ php/mailer.php
@@ -9,6 +9,7 @@
     $name = str_replace(array("\r","\n"),array(" "," "),$name);
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
     $message = trim($_POST["message"]);
+    $subject = "New website message from $name";
 
     // Check that data was sent to the mailer
     if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -20,20 +21,17 @@
     }
 
     // Build the email content
-    $email_content = "Name: $name\n";
-    $email_content .= "Email: $email\n\n";
-    $email_content .= "Message:\n$message\n";
-
-    // Build the email headers
-    $email_headers = "From: $name <$email>";
+    $content = "Name: $name\n";
+    $content .= "Email: $email\n\n";
+    $content .= "Message:\n$message\n";
 
     // Send the email
     try {
       $message = new Message();
       $message->setSender("admin@jrod.io");
       $message->addTo("john@jrod.io");
-      $message->setSubject("New website message from $name");
-      $message->setTextBody($message);
+      $message->setSubject($subject);
+      $message->setTextBody($content);
       $message->send();
       http_response_code(200);
       echo "Thank You!<br>Your message was delivered.";
@@ -42,9 +40,8 @@
       echo "Oops! Something went wrong on my end.<br>Your message wasn't delivered.";
     }
 
+  // Not a POST request, set a 403 (forbidden) response code
   } else {
-
-    // Not a POST request, set a 403 (forbidden) response code
     http_response_code(403);
     echo "Nice try, but I don't think so tough guy =P";
   }
